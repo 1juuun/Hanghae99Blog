d4e18989734f1eab768b9b78b6266eb5ed5460b1
1juuun <potenter11@gmail.com>
2023-02-21 21:53:16 +0900

    Fix: antMatchers URI 수정

--- src/main/java/com/sparta/hanghae99_blog/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/hanghae99_blog/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -43,9 +44,9 @@ public class WebSecurityConfig{
 
         httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        httpSecurity.authorizeRequests().antMatchers("/api/user/**").permitAll()
-                .antMatchers("/api/posts/**").permitAll()
-                .antMatchers("/api/comments/**").permitAll()
+        httpSecurity.authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/api/posts/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/comments/**").permitAll()
                 .anyRequest().authenticated()
                         .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
